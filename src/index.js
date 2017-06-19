2dd47b8c3b5412639ab2455931fb7d944e52f5bf
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-20 01:23:15 +0800

    react 2-19

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Clock extends Component {
+    constructor() {
+        super()
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentWillMount() {
+        this.timer = setInterval(() => {
+            this.setState({ date: new Date() })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    <p>現在的時間是</p>
+                    {this.state.date.toLocaleTimeString()}
+                </h1>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    constructor() {
+        super()
+        this.state = { isShowClock: true }
+    }
+
+    handleShowOrHide() {
+        this.setState({
+            isShowClock: !this.state.isShowClock
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isShowClock ? <Clock /> : null}
+                <button onClick={this.handleShowOrHide.bind(this)}>
+                    顯示或隱藏時鐘
+                </button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
