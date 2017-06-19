9c83460c6efc0f6ba51fef7ec56e8156ba03a4ae
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-19 14:30:36 +0800

    react 1-6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React 小书</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+)
