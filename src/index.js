03306941bdd1dc074245f8e16ba2ae84cba2b71e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-20 01:39:36 +0800

    react 2-22

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Card extends Component {
+    render() {
+        console.log(this.props.children)
+
+        return (
+            <div className='card'>
+                <div className='card-content'>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Card>
+        <h2>React.js 小书</h2>
+        <div>开源、免费、专业、简单</div>
+        订阅：<input />
+    </Card>,
+    document.getElementById('root')
+)
