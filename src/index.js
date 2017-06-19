7f4720b645d9305733b9cfd72fe961f1be19e9d6
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-19 16:32:19 +0800

    react 1-10

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class LikeButton extends Component {
+    constructor() {
+        super()
+        this.state = { isLiked: false }
+    }
+
+    handleClickOnLikeButton() {
+        this.setState({
+            isLiked: !this.state.isLiked
+        })
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClickOnLikeButton.bind(this)}>
+                {this.state.isLiked ? '取消' : '點讚'} 👍
+            </button>
+        )
+    }
+}
+
+ReactDOM.render(<LikeButton />, document.getElementById('root'));
