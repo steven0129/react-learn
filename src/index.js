e2d83293262f9e83b87d2a3b15eeaf1c4b144fb0
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-19 15:55:56 +0800

    react 1-7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    React 小书
+                    {this.renderGoodWord(
+                        <strong> is good</strong>,
+                        <span> is not good</span>
+                    )}
+                </h1>
+            </div>
+        )
+    }
+
+    renderGoodWord(goodWord, badWord) {
+        const isGoodWord = true
+        return isGoodWord ? goodWord : badWord
+    }
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+)
