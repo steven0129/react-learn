286b4a7de29c4346c81668a4c84244c94ddafa5a
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-19 18:00:24 +0800

    利用map函式渲染列表

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const users = [
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Lucy', age: 20, gender: 'female' }
+]
+
+class Index extends Component {
+    render() {
+        let usersElements = users.map((user) => {
+            return (
+                <div>
+                    <div>姓名：{user.username}</div>
+                    <div>年龄：{user.age}</div>
+                    <div>性别：{user.gender}</div>
+                    <hr />
+                </div>
+            )
+        })
+
+        return (
+            <div>{usersElements}</div>
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'))
