28aaf16ce20a4e89b90e6c2065fc1e5bfe401c4d
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-21 10:24:18 +0800

    react 3-29

--- src/index.js
+++ src/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is header</h2>
+                <Title />
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is main</h2>
+                <Content />
+            </div>
+        )
+    }
+}
+
+class Title extends Component {
+    static contextTypes = {
+        themeColor: PropTypes.string
+    }
+
+    render() {
+        return (
+            <h1 style={{ color: this.context.themeColor }}>React.js 小书标题</h1>
+        )
+    }
+}
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h2>React.js 小书内容</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    static childContextTypes = {
+        themeColor: PropTypes.string
+    }
+
+    constructor() {
+        super()
+        this.state = {
+            themeColor: 'red'
+        }
+    }
+
+    getChildContext() {
+        return {
+            themeColor: this.state.themeColor
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Main />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
