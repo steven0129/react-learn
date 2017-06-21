4a4cb4b4e4da5b81c1c72acd5256118927b70841
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-21 15:36:31 +0800

    使用redux, react-redux套件

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
-import { Provider } from './react-redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
-
-function createStore(reducer) {
-    let state = null
-    const listeners = []
-    const subscribe = (listener) => listeners.push(listener)
-    const getState = () => state
-    const dispatch = (action) => {
-        state = reducer(state, action)
-        listeners.forEach((listener) => listener())
-    }
-    dispatch({}) // 初始化 state
-    return { getState, dispatch, subscribe }
-}
 
 const themeReducer = (state, action) => {
     if (!state) return {
