6e03feb82100471c3f1af56ebe4dc9154e59ca0b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-06-21 22:56:37 +0800

    以babel驅動next.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import Link from 'next/link'
+import { observable, computed } from "mobx";
+
+class OrderLine {
+  @observable price = 0;
+  @observable amount = 1;
+  @computed get total() {
+    return this.price * this.amount;
+  }
+}
+
+const line = new OrderLine();
+console.log("price" in line); // true
+console.log(line.hasOwnProperty("price")); // true, 现在所有的属性都定义在实例上了。
 
 export default () => (
   <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
+    <li>1</li>
+    <li>1</li>
   </ul>
 )
